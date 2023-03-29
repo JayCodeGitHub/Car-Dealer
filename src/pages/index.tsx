5a4452696f523a0a80fbe5581e93e4f05436a5cf
JayCode Developer <JayCode.contakt@gmail.com>
2023-03-30 00:07:52 +0200

    Created Layout and moved threre NavBar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
-import NavBar from "@/components/navbar/navbar";
-
 export default function Home() {
   return (
     <main>
-      <NavBar />
       <h1>Car Deader</h1>
     </main>
   );
--- src/pages/offer.tsx
+++ src/pages/offer.tsx
@@ -1,9 +1,6 @@
-import NavBar from "@/components/navbar/navbar";
-
 export default function Offer() {
   return (
     <main>
-      <NavBar />
       <h1>Offer</h1>
     </main>
   );
