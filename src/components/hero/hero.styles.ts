ed0e5f3a33fc85e879dc7964537f83ec0ee4018a
JayCode Developer <JayCode.contakt@gmail.com>
2023-04-01 21:04:52 +0200

    Moved hero styles to separate file

---
+++ src/components/hero/hero.styles.ts
@@ -0,0 +1,86 @@
+export const heroStyles = {
+    wrapper: {
+        display: "flex",
+        paddingTop: "5rem",
+        paddingBottom: "5rem",
+    },
+    header: {
+        width: { xs: "100%", md: "50%" },
+        paddingTop: { xs: "5rem", md: "0" },
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: "2.5rem",
+        textAlign: "center",
+    },
+    description: {
+        mt: 3,
+        padding: "1rem",
+        fontSize: "1.1rem"
+    },
+    buttonsWrapper: {
+        width: { xs: "80%", md: "60%" },
+        display: "flex",
+        justifyContent: "space-around",
+    },
+    button: {
+        mt: 3,
+        backgroundColor: "#f50057",
+        border: "solid transparent 2px",
+        "&:hover": {
+          border: "solid #f50057 2px",
+          color: "black",
+          backgroundColor: "transparent",
+        },
+    },
+    secondButton: {
+        mt: 3,
+        backgroundColor: "transparent",
+        color: "black",
+        border: "solid #f50057 2px",
+        "&:hover": {
+          backgroundColor: "#f50057",
+          color: "white",
+        },
+    },
+    imageWrapper: {
+        width: "50%",
+        display: { xs: "none", md: "flex" } 
+    }
+  };
+
+export const secondaryHeroStyles = {
+    wrapper: {
+        backgroundImage: "url(./Ferrari.jpg)",
+        backgroundSize: "cover",
+        backgroundPositionY: "center",
+        position: "relative",
+        height: { xs: "12rem", md: "24rem" },
+    },
+    imageWrapper: {
+        backgroundColor: "rgba(24, 26, 27, 0.92)",
+        height: "100%",
+        position: "absolute",
+        right: "0",
+        top: "0",
+        width: "100%",
+    },
+    header: {
+        position: "relative",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+    },
+    title: {
+        mt: 3,
+        textAlign: "center",
+        fontSize: { xs: "2.8rem", md: "5rem" },
+        color: "white",
+        margin: "0",
+    }
+    
+  };
--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import { Box, Button, Container, Typography, CardMedia } from "@mui/material";
+import { heroStyles } from "./hero.styles";
+import { secondaryHeroStyles } from "./hero.styles";
 
 interface HeroProps {
   primary?: boolean;
@@ -28,60 +30,18 @@ export default function Hero({
         <Box>
           <Container maxWidth="xl">
             <header>
-              <Box
-                sx={{
-                  display: "flex",
-                  paddingTop: "5rem",
-                  paddingBottom: "5rem",
-                }}
-              >
-                <Box
-                  sx={{
-                    width: { xs: "100%", md: "50%" },
-                    paddingTop: { xs: "5rem", md: "0" },
-                    display: "flex",
-                    justifyContent: "center",
-                    flexDirection: "column",
-                  }}
-                >
-                  <Typography
-                    variant="h2"
-                    sx={{
-                      fontWeight: "bold",
-                      fontSize: "2.5rem",
-                      textAlign: "center",
-                    }}
-                  >
+              <Box sx={heroStyles.wrapper}>
+                <Box component="header" sx={heroStyles.header}>
+                  <Typography variant="h2" sx={heroStyles.title}>
                     {title}
                   </Typography>
-                  <Typography
-                    variant="body1"
-                    sx={{ mt: 3, padding: "1rem", fontSize: "1.1rem" }}
-                  >
+                  <Typography variant="body1" sx={heroStyles.description}>
                     {description}
                   </Typography>
-                  <Box
-                    sx={{
-                      width: { xs: "80%", md: "60%" },
-                      display: "flex",
-                      justifyContent: "space-around",
-                    }}
-                  >
+                  <Box sx={heroStyles.buttonsWrapper}>
                     {buttonHref ? (
                       <Link href={buttonHref}>
-                        <Button
-                          variant="contained"
-                          sx={{
-                            mt: 3,
-                            backgroundColor: "#f50057",
-                            border: "solid transparent 2px",
-                            "&:hover": {
-                              border: "solid #f50057 2px",
-                              color: "black",
-                              backgroundColor: "transparent",
-                            },
-                          }}
-                        >
+                        <Button variant="contained" sx={heroStyles.button}>
                           {button}
                         </Button>
                       </Link>
@@ -90,16 +50,7 @@ export default function Hero({
                       <Link href={secondButtonHref}>
                         <Button
                           variant="contained"
-                          sx={{
-                            mt: 3,
-                            backgroundColor: "transparent",
-                            color: "black",
-                            border: "solid #f50057 2px",
-                            "&:hover": {
-                              backgroundColor: "#f50057",
-                              color: "white",
-                            },
-                          }}
+                          sx={heroStyles.secondButton}
                         >
                           {secondButton}
                         </Button>
@@ -107,7 +58,7 @@ export default function Hero({
                     ) : null}
                   </Box>
                 </Box>
-                <Box sx={{ width: "50%", display: { xs: "none", md: "flex" } }}>
+                <Box sx={heroStyles.imageWrapper}>
                   <CardMedia
                     component="img"
                     sx={{ width: "100%" }}
@@ -120,45 +71,10 @@ export default function Hero({
           </Container>
         </Box>
       ) : (
-        <Box
-          sx={{
-            backgroundImage: "url(./Ferrari.jpg)",
-            backgroundSize: "cover",
-            backgroundPositionY: "center",
-            position: "relative",
-            height: { xs: "12rem", md: "24rem" },
-          }}
-        >
-          <Box
-            sx={{
-              backgroundColor: "rgba(24, 26, 27, 0.92)",
-              height: "100%",
-              position: "absolute",
-              right: "0",
-              top: "0",
-              width: "100%",
-            }}
-          />
-          <Box
-            component="header"
-            sx={{
-              position: "relative",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              height: "100%",
-            }}
-          >
-            <Typography
-              variant="h2"
-              sx={{
-                mt: 3,
-                textAlign: "center",
-                fontSize: { xs: "2.8rem", md: "5rem" },
-                color: "white",
-                margin: "0",
-              }}
-            >
+        <Box sx={secondaryHeroStyles.wrapper}>
+          <Box sx={secondaryHeroStyles.imageWrapper} />
+          <Box component="header" sx={secondaryHeroStyles.header}>
+            <Typography variant="h2" sx={secondaryHeroStyles.title}>
               {title}
             </Typography>
           </Box>
