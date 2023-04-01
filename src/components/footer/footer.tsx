3fc4c8e0d1cc61825e9342781861507193587f62
JayCode Developer <JayCode.contakt@gmail.com>
2023-04-01 20:26:59 +0200

    Moved footer Styles to separate file

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -7,31 +7,15 @@ import {
   ListItemText,
 } from "@mui/material";
 import { footerItems } from "../assets/items/footerItems";
+import { footerStyles } from "./footer.styles";
 
 export default function Footer() {
   return (
-    <Box
-      component="footer"
-      sx={{
-        backgroundColor: "#181A1B",
-        color: "white",
-        borderTop: "solid #f50057 2px",
-      }}
-    >
+    <Box component="footer" sx={footerStyles.outerWrapper}>
       <Container maxWidth="xl">
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-around",
-            padding: "2rem 0",
-            flexDirection: { xs: "column", md: "row" },
-          }}
-        >
+        <Box sx={footerStyles.wrapper}>
           {footerItems.map(({ title, list }) => (
-            <Box
-              sx={{ width: "20rem", paddingTop: { xs: "2rem", md: "0" } }}
-              key={title}
-            >
+            <Box sx={footerStyles.infoWrapper} key={title}>
               <Typography>{title}</Typography>
               <List>
                 {list.map((item) => (
