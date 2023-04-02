f819cb897f287073cfc44cb12a0227c5262d653c
JayCode Developer <JayCode.contakt@gmail.com>
2023-04-02 13:07:20 +0200

    Styled Contact Section

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Typography, List, ListItem, ListItemText } from "@mui/material";
 
 export default function ContactSection() {
   return (
@@ -7,13 +7,30 @@ export default function ContactSection() {
         sx={{
           width: { xs: "100%", md: "50%" },
           height: "35rem",
-          padding: "2rem",
+          padding: { xs: "2rem 2rem 4rem 2rem", md: "2rem 2rem 10rem 2rem" },
           display: "flex",
           flexDirection: "column",
           justifyContent: "space-around",
         }}
       >
-        <h1>Contact</h1>
+        <Typography variant="h3">Feel free to contact us!</Typography>
+        <Typography variant="body1">
+          At our dealership, we pride ourselves on providing top-notch customer
+          service. If you have any questions or concerns, please don&apos;t
+          hesitate to reach out to us. We offer various ways to contact us,
+          whether it&apos;s by phone, email, or filling out the contact form on
+          our website. Our team is dedicated to ensuring your satisfaction and
+          providing the highest level of service. Contact us today and
+          experience our exceptional service firsthand.
+        </Typography>
+        <List>
+          <ListItem>
+            <ListItemText primary="Email: example@example.com" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="Phone: +1234567890" />
+          </ListItem>
+        </List>
       </Box>
     </>
   );
