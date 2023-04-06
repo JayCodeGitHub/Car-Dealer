3c0b6e24c78adcbcb9f4fd2c58301ebcf9be9664
JayCode Developer <JayCode.contakt@gmail.com>
2023-04-06 19:37:36 +0200

    Added Content to About Page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -12,29 +12,29 @@ export default function About() {
     alt: "Image of Out Team",
   };
   const DealershipsSectionItems = {
-    img: "./team.jpg",
-    title: "Expert Team at Your Service",
+    img: "./dealership.jpeg",
+    title: "Network of Showrooms Across Europe",
     description:
-      "Our team is dedicated to providing personalized customer service and helping you find your perfect luxury or sports car. Trust us to guide you through the process and arrange financing options that work for you. Contact us to schedule a test drive and start your journey towards luxury driving.",
-    alt: "Image of Out Team",
+      "Experience luxury car buying in our high-end showrooms. Our top-notch service offers complimentary test drives and expert advice. Find our dealerships in major cities across Europe for an unforgettable in-store experience.",
+    alt: "Image of Out dealership",
   };
   return (
     <main>
       <Hero title="About" />
       <Container maxWidth="xl">
         <InfoSection
-          title={TeamSectionItems.title}
-          description={TeamSectionItems.description}
-          img={TeamSectionItems.img}
-          alt={TeamSectionItems.alt}
-        />
-        <FeatureSection />
-        <InfoSection
           secondary
           title={DealershipsSectionItems.title}
           description={DealershipsSectionItems.description}
           img={DealershipsSectionItems.img}
           alt={DealershipsSectionItems.alt}
+        />
+        <FeatureSection />
+        <InfoSection
+          title={TeamSectionItems.title}
+          description={TeamSectionItems.description}
+          img={TeamSectionItems.img}
+          alt={TeamSectionItems.alt}
         />
       </Container>
     </main>
