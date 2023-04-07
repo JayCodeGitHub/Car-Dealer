1ff097f3957cd7956bcb524344e68ed50016346d
JayCode Developer <JayCode.contakt@gmail.com>
2023-04-07 13:48:30 +0200

    Moved info Section Items to separate file

--- src/assets/items/mainPageItems.ts
+++ src/assets/items/mainPageItems.ts
@@ -1,12 +1,18 @@
-export const mainPageItems = 
-    {
-        heroItems: {
-            title: "Find Your Dream Car Today Browse Our Selection!",
-            description: "At our dealership, we offer the best selection of high-qualitycars, trucks, and SUVs at competitive prices. With a team ofknowledgeable and friendly sales professionals, we make it easyto find your dream car. Plus, our financing options and expertservice department ensure a hassle-free ownership experience.Visit us today and experience the difference!",
-            button: "See Offer",
-            buttonHref: "/offer",
-            secondButton: "Contact Us",
-            secondButtonHref: "/contact",
-            img: "./car.png"
-        }
-    }
+export const heroItems = {
+    title: "Find Your Dream Car Today Browse Our Selection!",
+    description: "At our dealership, we offer the best selection of high-qualitycars, trucks, and SUVs at competitive prices. With a team ofknowledgeable and friendly sales professionals, we make it easyto find your dream car. Plus, our financing options and expertservice department ensure a hassle-free ownership experience.Visit us today and experience the difference!",
+    button: "See Offer",
+    buttonHref: "/offer",
+    secondButton: "Contact Us",
+    secondButtonHref: "/contact",
+    img: "./car.png"
+}
+export const infoSectionItems = {
+    title: "Exceptional Service and Expertise",
+    description:
+      "Our team of experts is dedicated to providing you with the best sports car shopping experience. We take pride in offering exceptional customer service and professional guidance at every stage of the car buying process. With our extensive network of sales locations across Europe, we offer a wide range of high-performance vehicles to suit your preferences.",
+    img: "./dealership.jpg",
+    alt: "Modern and elegant car dealership with large windows showcasing a range of high-performance vehicles.",
+    button: "See More",
+    buttonHref: "/about",
+};
