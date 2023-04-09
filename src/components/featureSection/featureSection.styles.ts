5c8d72af69db9b347e3d2151a4cc352c7c4b14e8
JayCode Developer <JayCode.contakt@gmail.com>
2023-04-09 19:53:22 +0200

    Moved feature Section styles to separate file

---
+++ src/components/featureSection/featureSection.styles.ts
@@ -0,0 +1,40 @@
+export const featureSectionStyles = {
+    wrapper: {
+        display: "flex",
+        margin: { xs: "0 0 4rem 0", lg: "8rem 0" },
+        flexDirection: "column",
+        justifyContent: "space-around",
+        alignItems: "center",
+        gap: { xs: "2rem", lg: "4rem" },
+        padding: "0",
+    },
+    title: {
+        fontSize: { xs: "1.8rem", lg: "2rem", xl: "2.5rem" },
+        padding: { xs: "2rem 0", md: "0" },
+        fontWeight: "700",
+    },
+    stepsWrapper: {
+        width: "100%",
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+        flexDirection: { xs: "column", md: "row" },
+        gap: { xs: "5rem", md: "0" },
+    },
+    step: {
+        width: { xs: "90%", md: "25%" },
+        textAlign: "center",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-around",
+        gap: "1rem",
+    },
+    iconWrapper: {
+        color: "#f50057",
+        fontSize: "6rem"
+    },
+    description: {
+        color: "#706f7b" 
+    }
+  };
--- src/components/featureSection/featureSection.tsx
+++ src/components/featureSection/featureSection.tsx
@@ -1,5 +1,6 @@
 import { Container, Box, Typography } from "@mui/material";
 import { FeatureSectionItems } from "@/assets/items/featureSectionItems";
+import { featureSectionStyles } from "./featureSection.styles";
 
 export default function FeatureSection() {
   return (
@@ -7,52 +8,17 @@ export default function FeatureSection() {
       <Container
         component="div"
         maxWidth="xl"
-        sx={{
-          display: "flex",
-          margin: { xs: "0 0 4rem 0", lg: "8rem 0" },
-          flexDirection: "column",
-          justifyContent: "space-around",
-          alignItems: "center",
-          gap: { xs: "2rem", lg: "4rem" },
-          padding: "0",
-        }}
+        sx={featureSectionStyles.wrapper}
       >
-        <Typography
-          variant="h3"
-          sx={{
-            fontSize: { xs: "1.8rem", lg: "2rem", xl: "2.5rem" },
-            padding: { xs: "2rem 0", md: "0" },
-            fontWeight: "700",
-          }}
-        >
+        <Typography variant="h3" sx={featureSectionStyles.title}>
           {FeatureSectionItems.title}
         </Typography>
-        <Box
-          sx={{
-            width: "100%",
-            display: "flex",
-            justifyContent: "space-around",
-            alignItems: "center",
-            flexDirection: { xs: "column", md: "row" },
-            gap: { xs: "5rem", md: "0" },
-          }}
-        >
+        <Box sx={featureSectionStyles.stepsWrapper}>
           {FeatureSectionItems.steps.map(({ icon, title, description }) => (
-            <Box
-              key={title}
-              sx={{
-                width: { xs: "90%", md: "25%" },
-                textAlign: "center",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "space-around",
-                gap: "1rem",
-              }}
-            >
-              <Box sx={{ color: "#f50057", fontSize: "6rem" }}>{icon}</Box>
+            <Box key={title} sx={featureSectionStyles.step}>
+              <Box sx={featureSectionStyles.iconWrapper}>{icon}</Box>
               <Typography variant="h4">{title}</Typography>
-              <Typography variant="body1" sx={{ color: "#706f7b" }}>
+              <Typography variant="body1" sx={featureSectionStyles.description}>
                 {description}
               </Typography>
             </Box>
