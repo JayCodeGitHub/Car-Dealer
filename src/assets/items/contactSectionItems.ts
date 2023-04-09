ffbd8ae429d8453470bf8c76a62ab8d0ef6f1ce4
JayCode Developer <JayCode.contakt@gmail.com>
2023-04-09 20:07:50 +0200

    Moved contact section items to separate file

---
+++ src/assets/items/contactSectionItems.ts
@@ -0,0 +1,8 @@
+export const contactSectionItems = {
+    title: "Feel free to contact us!",
+    description: "At our dealership, we pride ourselves on providing top-notch customer service. If you have any questions or concerns, please don't hesitate to reach out to us. We offer various ways to contact us, whether it's by phone, email, or filling out the contact form on our website. Our team is dedicated to ensuring your satisfaction and providing the highest level of service. Contact us today and experience our exceptional service firsthand.",
+    email: "Email: example@example.com",
+    emailHref: "mailto:example@example.com",
+    tel: "Phone: +1234567890",
+    telHref: "tel:+1234567890",
+};
--- src/components/contactSection/contactSection.tsx
+++ src/components/contactSection/contactSection.tsx
@@ -1,33 +1,28 @@
 import { Box, Typography, List, ListItem, ListItemText } from "@mui/material";
 import { contactSectionStyles } from "./contactSection.styles";
+import { contactSectionItems } from "@/assets/items/contactSectionItems";
 
 export default function ContactSection() {
   return (
     <>
       <Box component="section" sx={contactSectionStyles.wrapper}>
-        <Typography variant="h3">Feel free to contact us!</Typography>
+        <Typography variant="h3">{contactSectionItems.title}</Typography>
         <Typography variant="body1">
-          At our dealership, we pride ourselves on providing top-notch customer
-          service. If you have any questions or concerns, please don&apos;t
-          hesitate to reach out to us. We offer various ways to contact us,
-          whether it&apos;s by phone, email, or filling out the contact form on
-          our website. Our team is dedicated to ensuring your satisfaction and
-          providing the highest level of service. Contact us today and
-          experience our exceptional service firsthand.
+          {contactSectionItems.description}
         </Typography>
         <List>
           <ListItem>
-            <a href="mailto:example@example.com">
+            <a href={contactSectionItems.emailHref}>
               <ListItemText
-                primary="Email: example@example.com"
+                primary={contactSectionItems.email}
                 sx={contactSectionStyles.listItem}
               />
             </a>
           </ListItem>
           <ListItem>
-            <a href="tel:+1234567890">
+            <a href={contactSectionItems.telHref}>
               <ListItemText
-                primary="Phone: +1234567890"
+                primary={contactSectionItems.tel}
                 sx={contactSectionStyles.listItem}
               />
             </a>
