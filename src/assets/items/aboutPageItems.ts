c04307a6985258fdf0783f9866c43f0444b70565
JayCode Developer <JayCode.contakt@gmail.com>
2023-04-10 16:47:23 +0200

    Fixed Images resolution on About page

--- src/assets/items/aboutPageItems.ts
+++ src/assets/items/aboutPageItems.ts
@@ -1,14 +1,14 @@
+export const DealershipsSectionItems = {
+  img: "./dealership2.webp",
+  title: "Network of Showrooms Across Europe",
+  description:
+  "Experience luxury car buying in our high-end showrooms. Our top-notch service offers complimentary test drives and expert advice. Find our dealerships in major cities across Europe for an unforgettable in-store experience.",
+  alt: "Image of Out dealership",
+};
 export const TeamSectionItems = {
-    img: "./team.jpg",
+    img: "./team.webp",
     title: "Expert Team at Your Service",
     description:
       "Our team is dedicated to providing personalized customer service and helping you find your perfect luxury or sports car. Trust us to guide you through the process and arrange financing options that work for you. Contact us to schedule a test drive and start your journey towards luxury driving.",
     alt: "Image of Out Team",
   };
-export const DealershipsSectionItems = {
-    img: "./dealership.jpeg",
-    title: "Network of Showrooms Across Europe",
-    description:
-      "Experience luxury car buying in our high-end showrooms. Our top-notch service offers complimentary test drives and expert advice. Find our dealerships in major cities across Europe for an unforgettable in-store experience.",
-    alt: "Image of Out dealership",
-};
